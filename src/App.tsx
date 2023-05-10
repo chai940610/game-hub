a1fc00ac2ff978eb808003060b29abf1dccaa48d
John <chai940610@hotmail.com>
2023-05-10 22:45:21 +0800

    Build a Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { Button, ButtonGroup, Grid, GridItem,Show } from '@chakra-ui/react'
+import NavBar from './components/NavBar'
 
 function App() {
-  return <Grid templateAreas={{
+  return <Grid templateAreas={{ //templateAreas is property that defines two different grid layouts,one for mobile and one for larger screen
     base:`"nav" "main"`,  //for mobile device
-    lg:`"nav nav" "aside main"`  //large devices, wider than 1024px
+    lg:`"nav nav" "aside main"`  //large devices, wider than 1024px, nav nav mean two nav column, aside main mean one aside one main
     }}>
-    <GridItem area='nav' bg='coral'>Nav</GridItem>
-    <Show above="lg">
+    <GridItem area='nav'>
+      <NavBar />
+    </GridItem>
+    <Show above="lg"> {/* show below means the aside will occur below 1024px, if above will show above 1024px */}
       <GridItem area='aside' bg='gold'>Aside</GridItem>
     </Show>
     <GridItem area='main' bg='dodgerblue'>Main</GridItem>
