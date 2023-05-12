939c9a8e9712e83649067352d75a81526b120e81
John <chai940610@hotmail.com>
2023-05-13 01:38:53 +0800

    Get optimized images

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -3,6 +3,7 @@ import { Game } from '../hooks/useGames'
 import { Card, CardBody, HStack, Heading, Image, Text } from '@chakra-ui/react'
 import PlatformIconList from './PlatformIconList'
 import CriticScore from './CriticScore'
+import getCroppedImageUrl from '../services/image-url'
 
 interface Props{
     game:Game
@@ -11,7 +12,7 @@ interface Props{
 const GameCard = ({game}:Props) => {
   return (
      <Card borderRadius={10} overflow='hidden'>  {/* why need overflow, because these image is too large, borderRadius is to let the corner not too sharp, when I set to 10, only the bottom change to not sharp, but upside left and right didn't, so need overflow */}
-        <Image src={game.background_image} />
+        <Image src={getCroppedImageUrl(game.background_image)} />  {/*we are downloading smaller images */}
         <CardBody>
             <Heading fontSize='2xl'>
                 {game.name} {/*this is game name, we can customize the name by using some features */}
