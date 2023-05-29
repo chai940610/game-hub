0dd6b24936322c3f2732ba5319869547992e1adc
John <chai940610@hotmail.com>
2023-05-29 14:41:24 +0800

    Clean up the genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -2,30 +2,48 @@ import useGenres, { Genre } from "../hooks/useGenres";
 import { Button, HStack, List, ListItem, Spinner } from "@chakra-ui/react";
 import { Image } from "@chakra-ui/react";
 import getCroppedImageUrl from "../services/image-url";
-import { Text } from "@chakra-ui/react";
+import { Heading } from "@chakra-ui/react";
 
-interface Props{
-  onSelectGenre:(genre:Genre)=>void;  //this function run when a genre is selected, and remember this function is require handlefunctionclick something like this to handle at front page.
-  kukubird:Genre|null;
+interface Props {
+  onSelectGenre: (genre: Genre) => void; //this function run when a genre is selected, and remember this function is require handlefunctionclick something like this to handle at front page.
+  kukubird: Genre | null;
 } //this whole props need bring to front handlecick something like this
 
-const GenreList = ({kukubird,onSelectGenre}:Props) => {  //{onSelectGenre}:Props} this function will be called when genre is selected
-  const { data,isLoading,error } = useGenres();
+const GenreList = ({ kukubird, onSelectGenre }: Props) => {
+  //{onSelectGenre}:Props} this function will be called when genre is selected
+  const { data, isLoading, error } = useGenres();
   if (error) return null; //if there is an error,return null
-  if (isLoading) return <Spinner />;  //if there is loading, return a spinner
+  if (isLoading) return <Spinner />; //if there is loading, return a spinner
   //once the data is loaded and no error occurs, the component renders below
 
   return (
-    <List>
-      {data.map((genre) => (
-        <ListItem key={genre.id} paddingY='5px'>
-          <HStack>
-            <Image boxSize='32px' borderRadius={8} src={getCroppedImageUrl(genre.image_background)} />
-            <Button fontWeight={genre.id===kukubird?.id?'bold':'normal'} onClick={()=>onSelectGenre(genre)} fontSize='lg' variant='link'>{genre.name}</Button>
-          </HStack>
-        </ListItem>
-      ))}
-    </List>
+    <>
+      <Heading fontSize='2xl' marginBottom={3}>Genres</Heading>
+      <List>
+        {data.map((genre) => (
+          <ListItem key={genre.id} paddingY="5px">
+            <HStack>
+              <Image
+                objectFit="cover"
+                boxSize="32px"
+                borderRadius={8}
+                src={getCroppedImageUrl(genre.image_background)}
+              />
+              <Button
+                textAlign="left"
+                whiteSpace="normal"
+                fontWeight={genre.id === kukubird?.id ? "bold" : "normal"}
+                onClick={() => onSelectGenre(genre)}
+                fontSize="lg"
+                variant="link"
+              >
+                {genre.name}
+              </Button>
+            </HStack>
+          </ListItem>
+        ))}
+      </List>
+    </>
   );
 };
 
