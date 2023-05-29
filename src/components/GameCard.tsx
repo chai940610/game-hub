a0411f1f9dc76f335fafd74c897f1fa9292fd952
John <chai940610@hotmail.com>
2023-05-29 15:29:19 +0800

    clean up the game cards

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -13,14 +13,14 @@ const GameCard = ({game}:Props) => {
   return (
      <Card >  {/* why need overflow, because these image is too large, borderRadius is to let the corner not too sharp, when I set to 10, only the bottom change to not sharp, but upside left and right didn't, so need overflow */}
         <Image src={getCroppedImageUrl(game.background_image)} />  {/*we are downloading smaller images, game.background_image exaple link, https://media.rawg.io/media/games/456/456dea5e1c7e3cd07060c14e96612001.jpg */} 
-        <CardBody>
-            <Heading fontSize='2xl'>
-                {game.name} {/*this is game name, we can customize the name by using some features */}
-            </Heading>
-            <HStack justifyContent='space-between'>
+        <CardBody>            
+            <HStack justifyContent='space-between' marginBlock={3}>
                 <PlatformIconList platforms={game.parent_platforms.map(p=>p.platform)} />
                 <CriticScore score={game.metacritic} />
             </HStack>
+            <Heading fontSize='2xl'>
+                {game.name} {/*this is game name, we can customize the name by using some features */}
+            </Heading>
         </CardBody>
     </Card>
   )
